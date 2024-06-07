1ee03435d272e1c56bec7734370c1abc6d966a16
Steven Lee <steven.lee@kynrai.com>
2024-06-07 20:07:27 +0100

    FEAT: add cors to nethttp server (#11)

--- middleware/cors.go
+++ middleware/cors.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,4 +24,23 @@ func CORSMiddleware() gin.HandlerFunc {
 			ctx.Next()
 		}
 	}
+}
+
+func CORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: determine allowed origins
+		w.Header().Set("Access-Control-Allow-Origin", "http://artwork.local:3000")
+		w.Header().Set("Access-Control-Max-Age", "86400")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, api_key, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Cache-Control", "no-cache")
+
+		if r.Method == http.MethodOptions {
+			w.Write([]byte{})
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
