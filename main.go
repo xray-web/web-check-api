73ca5a39c16c944aede0e40211fb4333c34c479b
Steven Lee <steven.lee@kynrai.com>
2024-06-07 19:51:35 +0100

    FEAT: use nethttp server, test with hurl
    - actiavte the nethttp server
    - basic tests with hurl scripts

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/xray-web/web-check-api/config"
-	"github.com/xray-web/web-check-api/routers"
+	"github.com/xray-web/web-check-api/server"
 )
 
 func main() {
-	router := routers.Routes()
-	config.ServerConfig(router)
+	s := server.New(config.New())
+	log.Println(s.Run())
+	// router := routers.Routes()
+	// config.ServerConfig(router)
 }
