abaf2611d8c7de33156a68b91042de23d4eb37b2
Steven Lee <steven.lee@kynrai.com>
2024-06-10 13:37:24 +0100

    FEAT: update allow origin from vars, limit methods (#35)

--- server/middleware.go
+++ server/middleware.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 )
 
-func CORS(h http.Handler) http.Handler {
+func (s *Server) CORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO: determine allowed origins
-		w.Header().Set("Access-Control-Allow-Origin", "http://artwork.local:3000")
+		w.Header().Set("Access-Control-Allow-Origin", s.conf.AllowedOrigin)
 		w.Header().Set("Access-Control-Max-Age", "86400")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, api_key, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Cache-Control", "no-cache")
