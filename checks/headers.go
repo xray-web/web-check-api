9bc9d1f33339e329c49d325a8752bd435bdd8503
Steven Lee <steven.lee@kynrai.com>
2024-06-15 12:44:57 +0100

    FIX: fix headers for multi strings (#51)
    - Return the native http.Headers object

--- checks/headers.go
+++ checks/headers.go
@@ -13,7 +13,7 @@ func NewHeaders(client *http.Client) *Headers {
 	return &Headers{client: client}
 }
 
-func (h *Headers) List(ctx context.Context, url string) (map[string]string, error) {
+func (h *Headers) List(ctx context.Context, url string) (http.Header, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
@@ -25,12 +25,5 @@ func (h *Headers) List(ctx context.Context, url string) (map[string]string, erro
 	}
 	defer resp.Body.Close()
 
-	responseHeaders := make(map[string]string)
-	for k, v := range resp.Header {
-		for _, s := range v {
-			responseHeaders[k] = s
-		}
-	}
-
-	return responseHeaders, nil
+	return resp.Header, nil
 }
